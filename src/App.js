5c51ddcda8c5923faa280bf985500e3cac0622da
Alicianonkonyana <133217174+Alicianonkonyana@users.noreply.github.com>
2024-05-19 07:03:55 +0200

    added a footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,32 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/Alicianonkonyana"
+          target="_black"
+          rel="noreferrer"
+        >
+          Alicia Nonkonyana
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Alicianonkonyana/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open sourced on Git-hub
+        </a>{" "}
+        hosted on
+        <a
+          href="https://tiny-hummingbird-564b6e.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
