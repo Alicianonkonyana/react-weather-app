dde08adaf450f1ec72c99a172e3cc46c27520498
Alicianonkonyana <133217174+Alicianonkonyana@users.noreply.github.com>
2024-05-22 19:11:23 +0200

    added Css to the weather app

--- src/Weather.js
+++ src/Weather.js
@@ -4,32 +4,38 @@ export default function Weather() {
   return (
     <div className="Weather">
       <div className="container">
-        <form>
-          {" "}
-          <input type="search" placeholder="type a city..."></input>
-          <input type="submit" value="search"></input>
+        <form className="Search-form">
+          <input
+            type="search"
+            placeholder="type a city..."
+            className="Form-control"
+            autoFocus
+          ></input>
+
+          <input type="submit" value="search" className="Submit-button" />
         </form>
+        <div className="Current-weather">
+          <h1>New York</h1>
+
+          <ul>
+            <li>Tuesday 12:16,clear sky</li>
+            <li>Humidity:86%,Wind :3.09 km/h</li>
+          </ul>
+        </div>
         <div className="row">
-          <div className="col-3">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-              alt="sunny icon"
-            />
+          <div className="col-6">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" />
+            <strong>
+              <span className="Temperature">6 </span>{" "}
+            </strong>
+            <span className="Temperature-unit">°C</span>
           </div>
-          <div className="col-3">
-            <h1>14°C</h1>
-          </div>
-          <div className="col-3">
+          <div className="col-6">
             <ul>
-              <li>precipitation:68%</li>
-              <li> Humidity:58%</li>
-              <li>Wind:39 km/h</li>
+              <li>precipitation:60%</li>
+              <li>Humidity:5%</li>
+              <li>Wind:5 km/h</li>
             </ul>
-          </div>
-          <div className="col-3">
-            <h2>Weather</h2>
-            <div>Monday 04:24</div>
-            <div>Clear</div>
           </div>
         </div>
       </div>
